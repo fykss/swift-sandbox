fadf0f756fbeef4344b1b0d36d92a8d7c633a7d6
Влад Летун <fyks1649@gmail.com>
2020-03-21 14:10:10 +0200

    Initial Commit

---
+++ Swift-Sandbox/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Sandbox
+//
+//  Created by Vladyslav Lietun on 21.03.2020.
+//  Copyright © 2020 Vladyslav Lietun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
