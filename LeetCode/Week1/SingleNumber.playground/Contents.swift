8f79ed92df4dab2fb7e7c7ef956abb7ef0aa7eb1
Vladyslav Lietun <fyks1649@gmail.com>
2020-04-27 14:59:00 +0300

    New solution Single Number

--- LeetCode/Week1/SingleNumber.playground/Contents.swift
+++ LeetCode/Week1/SingleNumber.playground/Contents.swift
@@ -11,6 +11,20 @@ func singleNumber(_ nums: [Int]) -> Int {
     return 0
 }
 
-print(singleNumber([1, 3, 1, 3, 2]))
+// Interest solution
+func singleNumberInt(_ nums: [Int]) -> Int {
+    return 2 * Set(nums).reduce(0, +) - nums.reduce(0, +)
+}
+
+var case1 = [1, 3, 1, 3, 2]
+var case2 = [2, 2, 1]
+var case3 = [5, 2, 2, 5, 10, 10, -1]
+
+assert(singleNumber(case1) == 2, "Invalid case 1")
+assert(singleNumber(case2) == 1, "Invalid case 2")
+assert(singleNumber(case3) == -1, "Invalid case 2")
+
+print("Test completed")
+
 
 
