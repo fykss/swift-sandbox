ea0d4d95f9267c4fd79ed661351a79ac062dfa8c
Vladyslav Lietun <fyks1649@gmail.com>
2020-04-27 16:32:00 +0300

    Completed Maximum Subarray

--- LeetCode/Week1/MaximumSubarray.playground/Contents.swift
+++ LeetCode/Week1/MaximumSubarray.playground/Contents.swift
@@ -1,3 +1,14 @@
-import UIKit
+func maxSubArray(_ nums: [Int]) -> Int {
+    var dp = nums
+    for i in 1..<dp.count {
+        dp[i] = [dp[i], dp[i-1] + dp[i]].max()!
+    }
+    return dp.max()!
+}
 
-var str = "Hello, playground"
+// Describe [-2, 1, -2, 4,  3, 5, 6,  1, 5]
+var case1 = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+
+assert(maxSubArray(case1) == 6, "Invalid case 1")
+
+print("Test completed")
